879446886539bf3c8015f4580f3090bc372df56e
anilbidari <anil.bidari@thecloudenabled.com>
2021-05-06 19:53:30 +0530

    Delete cluster.tf

--- cluster.tf
+++
@@ -1,6 +0,0 @@
-resource "google_container_cluster" "gke-cluster" {
-  name               = "my-first-gke-cluster"
-  network            = "default"
-  location           = "us-central1-a"
-  initial_node_count = 1
-}
